3251b79eeae9807d6c60044ac2f3b8cd80fec5b0
이종경 <gyoung3063413@naver.com>
2024-03-22 22:14:36 +0900

    Fix: 불필요한 코드 삭제

--- conf/webpack.common.js
+++ conf/webpack.common.js
@@ -13,8 +13,7 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        include: path.resolve(__dirname, "src"),
-        use: ["style-loader", "css-loader", "postcss-loader"],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
