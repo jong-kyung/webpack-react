5e7a130f5d0ce2cb36a91e2a22042db95c557f46
이종경 <gyoung3063413@naver.com>
2024-03-23 00:37:36 +0900

    Add: ESLint 설치 및 설정

---
+++ .eslintrc.js
@@ -0,0 +1,68 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    ecmaVersion: "latest",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/strict",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["react", "react-hooks", "@typescript-eslint", "jsx-a11y"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "@typescript-eslint/lines-between-class-members": [
+      "error",
+      {
+        enforce: [
+          {
+            blankLine: "always",
+            prev: "*",
+            next: "method",
+          },
+          {
+            blankLine: "always",
+            prev: "method",
+            next: "*",
+          },
+          {
+            blankLine: "always",
+            prev: "field",
+            next: "field",
+          },
+        ],
+      },
+      {
+        exceptAfterSingleLine: true,
+      },
+    ],
+    "no-underscore-dangle": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [
+      "warn",
+      {
+        extensions: [".ts", ".tsx"],
+      },
+    ],
+    "no-useless-catch": "off",
+  },
+};
