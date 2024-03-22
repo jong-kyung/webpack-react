e537719ce7c67a29d5468cc26011f676b9536638
이종경 <gyoung3063413@naver.com>
2024-03-22 22:06:26 +0900

    Add: webpack module에 css loader 추가

--- conf/webpack.common.js
+++ conf/webpack.common.js
@@ -11,6 +11,11 @@ module.exports = {
         exclude: /node_modules/,
         use: "babel-loader",
       },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
     ],
   },
   plugins: [
