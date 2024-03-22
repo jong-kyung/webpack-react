14c81d5baa78518c936107cdf27de91f51f21159
이종경 <gyoung3063413@naver.com>
2024-03-22 20:24:30 +0900

    Add: Typescript 설치

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 const container = document.getElementById("root");
-const root = createRoot(container);
+const root = createRoot(container as Element);
 
 root.render(<App />);
