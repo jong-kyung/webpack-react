d9beb2b826a4a14e57948e83c66fb246b38b13a8
이종경 <gyoung3063413@naver.com>
2024-03-26 17:48:13 +0900

    Fix: webpack.common.js CSS규칙 추가

--- conf/webpack.common.js
+++ conf/webpack.common.js
@@ -1,37 +1,37 @@
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.tsx",
-  module: {
-    rules: [
-      {
-        test: /\.(tsx|ts|jsx|js)$/,
-        exclude: /node_modules/,
-        use: "babel-loader",
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-    new webpack.ProvidePlugin({
-      React: "react",
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@pages": path.resolve(__dirname, "..", "src", "pages"),
-      "@components": path.resolve(__dirname, "..", "src", "components"),
-      "@containers": path.resolve(__dirname, "..", "src", "containers"),
-      "@types": path.resolve(__dirname, "..", "src", "utils"),
-    },
-    extensions: [".js", ".ts", ".jsx", ".tsx"],
-  },
+	entry: './src/index.tsx',
+	module: {
+		rules: [
+			{
+				test: /\.(tsx|ts|jsx|js)$/,
+				exclude: /node_modules/,
+				use: 'babel-loader'
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader', 'postcss-loader']
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html'
+		}),
+		new webpack.ProvidePlugin({
+			React: 'react'
+		})
+	],
+	resolve: {
+		alias: {
+			'@pages': path.resolve(__dirname, '..', 'src', 'pages'),
+			'@components': path.resolve(__dirname, '..', 'src', 'components'),
+			'@containers': path.resolve(__dirname, '..', 'src', 'containers'),
+			'@types': path.resolve(__dirname, '..', 'src', 'utils')
+		},
+		extensions: ['.js', '.ts', '.jsx', '.tsx']
+	}
 };
